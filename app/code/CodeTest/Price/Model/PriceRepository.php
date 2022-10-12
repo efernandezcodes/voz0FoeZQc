6b47a853049304b8833acffc651dde02b6fee492
Edward Fernandez <efernandez.codes@gmail.com>
2022-10-12 12:14:27 -0400

    Add source repository mapping to price repository

--- app/code/CodeTest/Price/Model/PriceRepository.php
+++ app/code/CodeTest/Price/Model/PriceRepository.php
@@ -4,23 +4,74 @@ namespace CodeTest\Price\Model;
 
 use CodeTest\Price\Api\Data\PriceInterface;
 use CodeTest\Price\Api\PriceRepositoryInterface;
+use CodeTest\Price\Exception\PriceException;
+use CodeTest\Price\Model\PriceInformation\ConfigInterface;
 use Magento\Framework\Exception\LocalizedException;
 
 class PriceRepository implements PriceRepositoryInterface
 {
+    const SOURCE_REPO_PRICE_INFORMATION = 'price_info';
+    const SOURCE_REPO_PRODUCT = 'product';
+
+    protected ?PriceRepositoryInterface $sourceRepository = null;
+
     /**
-     * @param PriceRepositoryInterface|null $sourceRepository
-     * @throws LocalizedException
+     * @param ConfigInterface $config
+     * @param array|null $sourceMap
+     *
+     * @author Edward Fernandez <efernandez.codes@gmail.com>
      */
-    public function __construct(protected ?PriceRepositoryInterface $sourceRepository = null)
-    {
-        if ($this->sourceRepository === null) {
-            throw new LocalizedException(__('Source repository is not defined.'));
-        }
+    public function __construct(
+        protected ConfigInterface $config,
+        protected ?array $sourceMap = null
+    ) {
     }
 
+    /**
+     * @param int $productId
+     *
+     * @return PriceInterface|null
+     * @throws LocalizedException
+     * @throws PriceException
+     *
+     * @author Edward Fernandez <efernandez.codes@gmail.com>
+     */
     public function get(int $productId): ?PriceInterface
     {
-        return $this->sourceRepository->get($productId);
+        $sourceRepository = $this->resolveSourceRepository();
+        if ($sourceRepository === null) {
+            throw new LocalizedException(__('Source repository is not defined.'));
+        }
+
+        return $sourceRepository->get($productId);
+    }
+
+    /**
+     * @return PriceRepositoryInterface|null
+     * @throws LocalizedException
+     *
+     * @author Edward Fernandez <efernandez.codes@gmail.com>
+     */
+    protected function resolveSourceRepository(): ?PriceRepositoryInterface
+    {
+        if ($this->sourceRepository === null) {
+            $sourceType = $this->config->isEnabled() ? self::SOURCE_REPO_PRICE_INFORMATION : self::SOURCE_REPO_PRODUCT;
+            if (!isset($this->sourceMap[$sourceType])) {
+                throw new LocalizedException(
+                    __("The %1 value wasn't found in the source repositories.", $sourceType)
+                );
+            }
+
+            $sourceRepository = $this->sourceMap[$sourceType];
+            if (!$sourceRepository instanceof PriceRepositoryInterface) {
+                throw new LocalizedException(
+                    __('Given source repository must implement "%1"', PriceRepositoryInterface::class)
+                );
+            }
+
+            $this->sourceRepository = $sourceRepository;
+        }
+
+        return $this->sourceRepository;
     }
 }
--- app/code/CodeTest/PriceGraphQl/Model/Resolver/Price.php
+++ app/code/CodeTest/PriceGraphQl/Model/Resolver/Price.php
@@ -3,6 +3,7 @@
 namespace CodeTest\PriceGraphQl\Model\Resolver;
 
 use CodeTest\Price\Api\PriceRepositoryInterface;
+use CodeTest\Price\Exception\PriceException;
 use CodeTest\PriceGraphQl\Api\GraphQlPriceResultProcessorInterface;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
@@ -24,24 +25,32 @@ class Price implements ResolverInterface
      * @param ResolveInfo $info
      * @param array|null $value
      * @param array|null $args
+     *
      * @return array|null
      * @throws GraphQlInputException
+     * @noinspection PhpUnusedLocalVariableInspection
+     *
+     * @author Edward Fernandez <efernandez.codes@gmail.com>
      */
-    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): ?array
     {
         if (!isset($args['product_id'])) {
             throw new GraphQlInputException(__('Required parameter "product_id" is missing.'));
         }
         $productId = (int)$args['product_id'];
 
-        $price = $this->priceRepository->get($productId);
-
-        if ($price === null) {
+        try {
+            $price = $this->priceRepository->get($productId);
+            if ($price === null) {
+                return null;
+            }
+        } catch (PriceException $e) {
             return null;
         }
 
         $result = [];
         $this->resultProcessor->process($price, $result);
+
         return $result;
     }
 
