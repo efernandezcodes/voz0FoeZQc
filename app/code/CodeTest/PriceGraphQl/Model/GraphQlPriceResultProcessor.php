12bca2e9d1edc37435048b34b4eb35867632ce83
Edward Fernandez <efernandez.codes@gmail.com>
2022-10-12 12:15:33 -0400

    Add check for class type of GraphQL price processors

--- app/code/CodeTest/PriceGraphQl/Model/GraphQlPriceResultProcessor.php
+++ app/code/CodeTest/PriceGraphQl/Model/GraphQlPriceResultProcessor.php
@@ -4,6 +4,7 @@ namespace CodeTest\PriceGraphQl\Model;
 
 use CodeTest\Price\Api\Data\PriceInterface;
 use CodeTest\PriceGraphQl\Api\GraphQlPriceResultProcessorInterface;
+use Magento\Framework\Exception\LocalizedException;
 
 class GraphQlPriceResultProcessor implements GraphQlPriceResultProcessorInterface
 {
@@ -14,9 +15,24 @@ class GraphQlPriceResultProcessor implements GraphQlPriceResultProcessorInterfac
     {
     }
 
+    /**
+     * @param PriceInterface $price
+     * @param array $graphQlResult
+     *
+     * @return void
+     * @throws LocalizedException
+     *
+     * @author Edward Fernandez <efernandez.codes@gmail.com>
+     */
     public function process(PriceInterface $price, array &$graphQlResult): void
     {
         foreach ($this->processors as $processor) {
+            if (!$processor instanceof GraphQlPriceResultProcessorInterface) {
+                throw new LocalizedException(
+                    __('Given processor must implement "%1"', GraphQlPriceResultProcessorInterface::class)
+                );
+            }
+
             $processor->process($price, $graphQlResult);
         }
     }
